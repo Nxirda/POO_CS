12d492afb2b5f8d859505595347b5ea98c9ccf6a
Nxirda <nxirda@Myrkr.home>
2023-12-26 23:44:19 +0100

    CLEANUP [26/12]
    - Suppression fichiers innutiles

--- Projet_Simulation/release/Test[1]_include.cmake
+++
@@ -1,5 +0,0 @@
-if(EXISTS "/home/nxirda/Desktop/POO_CS/Projet_Simulation/release/Test[1]_tests.cmake")
-  include("/home/nxirda/Desktop/POO_CS/Projet_Simulation/release/Test[1]_tests.cmake")
-else()
-  add_test(Test_NOT_BUILT Test_NOT_BUILT)
-endif()
